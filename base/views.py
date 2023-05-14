2bbfd5d34fc3c203b84545e5ab3a4f5570777d54
tommy <tomas.nagy.tn@gmail.com>
2023-05-14 21:58:27 +0200

    create topic and message models

--- base/views.py
+++ base/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
+from .models import Group
 
 # Create your views here.
 
 
-groups = [
-    {'id': 1, 'name': "Let's learn Python"},
-    {'id': 2, 'name': "Let's learn JavaScript"},
-    {'id': 3, 'name': "Let's learn C++"},
-    {'id': 4, 'name': "Let's learn Rust"},
-]
-
-
 def home(request):
+    groups = Group.objects.all()
     context = {'groups': groups}
     return render(request, 'base/home.html', context)
 
 
 def group(request, pk):
-    group = None
-    for i in groups:
-        if i['id'] == int(pk):
-            group = i
+    group = Group.objects.get(id=pk)
 
     context = {'group': group}
 
